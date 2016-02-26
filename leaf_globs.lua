9f0910c8b55273b0b491f01e04afb14ca7b2e930
Tyler Neylon <tylerneylon@gmail.com>
2016-02-25 21:49:56 -0800

    Begin the leaf_globs module.

---
+++ leaf_globs.lua
@@ -0,0 +1,88 @@
+--[[
+
+leaf_globs.lua
+
+A module to build leaf globs.
+
+--]]
+
+local leaf_globs = {}
+
+-- TODO Are these both needed?
+local Mat3 = require 'Mat3'
+local Vec3 = require 'Vec3'
+
+
+-- Internal functions.
+
+local function add_lin_indep_pt(pts)
+  -- TODO Implement.
+end
+
+local function opposite(pt)
+  assert(getmetatable(pt) == Vec3)
+  return pt * -1
+end
+
+local function rand_pt_in_triangle(t)
+  -- TODO Implement.
+end
+
+
+-- Public functions.
+
+-- Inputs: center is a Vec3
+--         radius is a number
+--         triangles is an optional sequence table; new triangles
+--                   will be appended to this if it is present
+-- Outputs: a sequence table with a flat vertex array of triangle corners
+-- The output is designed to be usable as an input to VertexArray:new.
+function leaf_globs.make_glob(center, radius, out_triangles)
+  out_triangles = out_triangles or {}
+
+  -- This will be a sequence of Vec3 points on the unit sphere. We'll try to
+  -- make the first 3 linearly independent, and choose the last so that the
+  -- tetrahedron we've formed includes the origin.
+  local init_pts = {}
+  for i = 1, 3 do
+    add_lin_indep_pt(init_pts)
+  end
+  local opposite_triangle = {}
+  for i = 1, 3 do table.insert(opposite_triangle, opposite(init_pts[i])) end
+  table.insert(init_pts, rand_pt_in_triangle(opposite_triangle))
+
+  -- This is a sequence of triangles. Each triangle is a triple of Vec3 points,
+  -- ordered counterclockwise when viewed from the outside.
+  -- For now, all points are centered at the origin. We'll translate by center
+  -- just before values are appended to out_triangles.
+  local glob_triangles = {}
+
+  -- Transfer the initial points to our glob_triangles sequence.
+  assert(#init_pts == 4)
+  for i = 1, 4 do
+    -- Insert the triangle that omits init_pts[i].
+    -- We have to be careful to put the points in counterclockwise order.
+    local pts = init_pts  -- I like short names.
+    local t = {pts[i % 4 + 1], pts[(i + 1) % 4 + 1], pts[(i + 2) % 4 + 1]}
+    if normal(t):dot(t[1]) < 0 then
+      -- We need to switch the order.
+      t[2], t[3] = t[3], t[2]
+    end
+    table.insert(glob_triangles, t)
+  end
+
+  -- TODO Make the globs more interesting.
+
+  -- TODO Finish this implementation.
+
+  for _, t in pairs(glob_triangles) do
+  end
+
+
+
+end
+
+
+return leaf_globs
+
+
